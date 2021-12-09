d9b5c42eec35c4cc1859d60735431d6119175ee5
Thu Nguyen <hathu0610@gmail.com>
2021-12-09 22:17:51 +0700

    image optimize + it work

--- app/js/script.js
+++ app/js/script.js
@@ -24,8 +24,19 @@ btnHamburger.addEventListener('click', function() {
     }
 });
 
-function smoothScroll(){
-    document.querySelector('.a').scrollIntoView({
-        behavior: 'smooth'
-    });
+var accItem = document.getElementsByClassName('accordionItem');
+var accHD = document.getElementsByClassName('accordionItemHeading');
+for (i = 0; i < accHD.length; i++) {
+    accHD[i].addEventListener('click', toggleItem, false);
 }
+function toggleItem() {
+    var itemClass = this.parentNode.className;
+    for (i = 0; i < accItem.length; i++) {
+        accItem[i].className = 'accordionItem close';
+    }
+    if (itemClass == 'accordionItem close') {
+        this.parentNode.className = 'accordionItem open';
+    }
+}
+
+
